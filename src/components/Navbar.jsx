05feae8a2bfaac35f6bade985b66b67db27a3106
SumanJK <96579301+SumanJK@users.noreply.github.com>
2022-08-24 23:47:18 +0530

    updated navBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,10 +15,13 @@ import {
 import React from "react";
 import { CgMenuLeft, CgSearch } from "react-icons/cg";
 import { IoMdNotificationsOutline } from "react-icons/io";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 import DrewAvatar from "../assets/profile-pic-02.jpg";
 import AlexaAvatar from "../assets/profile-pic-06.jpg";
 import EvaAvatar from "../assets/profile-pic-05.jpg";
+import profilePic from "../assets/profile-pic-01.jpg";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 const Navbar = () => {
   return (
@@ -66,108 +69,138 @@ const Navbar = () => {
         className="rightNav"
         justify="space-between"
         w="12%"
-        // border="1px solid black"
+        border="1px solid black"
       >
-        <Menu>
-          <MenuButton
-            size="xs"
-            as={Button}
-            rounded={"full"}
-            cursor={"pointer"}
-            bg="#fff"
-            _hover={{ bg: "#f0f0f0" }}
-            _active={{ bg: "#f0f0f0" }}
-            position="relative"
-          >
-            <IoMdNotificationsOutline fontSize="20px" color="#c3c3c3" />
-            <Icon viewBox="0 0 200 200" color="green.400" position="absolute" top='0'>
-              <path
-                fill="currentColor"
-                d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"
-              />
-            </Icon>
+        <Flex className="notificationDiv">
+          <Menu>
+            <MenuButton
+              size="xs"
+              as={Button}
+              rounded={"full"}
+              cursor={"pointer"}
+              bg="#fff"
+              _hover={{ bg: "#f0f0f0" }}
+              _active={{ bg: "#f0f0f0" }}
+              position="relative"
+            >
+              <IoMdNotificationsOutline fontSize="20px" color="#c3c3c3" />
+              <Icon
+                viewBox="0 0 200 200"
+                color="green.400"
+                position="absolute"
+                top="0"
+              >
+                <path
+                  fill="currentColor"
+                  d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"
+                />
+              </Icon>
+            </MenuButton>
+            <MenuList w="22rem" border="2px soli black" mt={"1rem"}>
+              <MenuItem h="76px" _hover={{ bg: "#f5faff" }}>
+                <Flex justify="space-between" w="100%">
+                  <Center w="15%">
+                    <Avatar
+                      h="2.4rem"
+                      w="2.4rem"
+                      name="Drew"
+                      src={DrewAvatar}
+                    />
+                  </Center>
+                  <Box w="70%" color="#43425D" p="0 .5rem">
+                    <Text
+                      fontSize="14px"
+                      letterSpacing="0px"
+                      color="#4D4F5C"
+                      fontWeight={"600"}
+                    >
+                      Bravid Kames
+                    </Text>
+                    <Text opacity="50%" fontSize="12px">
+                      Assigned you on the call with Sara
+                    </Text>
+                  </Box>
+                  <Box w="20%">
+                    <Text opacity="50%" fontSize="12px">
+                      2 min ago
+                    </Text>
+                  </Box>
+                </Flex>
+              </MenuItem>
+              <MenuItem h="76px" _hover={{ bg: "#f5faff" }}>
+                <Flex justify="space-between" w="100%">
+                  <Center w="15%">
+                    <Avatar
+                      h="2.4rem"
+                      w="2.4rem"
+                      name="Alexa"
+                      src={AlexaAvatar}
+                    />
+                  </Center>
+                  <Box w="70%" color="#43425D" p="0 .5rem">
+                    <Text
+                      fontSize="14px"
+                      letterSpacing="0px"
+                      color="#4D4F5C"
+                      fontWeight={"600"}
+                    >
+                      Alexa Marry
+                    </Text>
+                    <Text opacity="50%" fontSize="12px">
+                      Marked the task New UI as done
+                    </Text>
+                  </Box>
+                  <Box w="20%">
+                    <Text opacity="50%" fontSize="12px">
+                      5 min ago
+                    </Text>
+                  </Box>
+                </Flex>
+              </MenuItem>
+              <MenuItem h="76px" _hover={{ bg: "#f5faff" }}>
+                <Flex justify="space-between" w="100%">
+                  <Center w="15%">
+                    <Avatar h="2.4rem" w="2.4rem" name="Eva" src={EvaAvatar} />
+                  </Center>
+                  <Box w="70%" color="#43425D" p="0 .5rem">
+                    <Text
+                      fontSize="14px"
+                      letterSpacing="0px"
+                      color="#4D4F5C"
+                      fontWeight={"600"}
+                    >
+                      Eva Maria
+                    </Text>
+                    <Text opacity="50%" fontSize="12px">
+                      Added a new comment on Sales task
+                    </Text>
+                  </Box>
+                  <Box w="20%">
+                    <Text opacity="50%" fontSize="12px">
+                      10 min ago
+                    </Text>
+                  </Box>
+                </Flex>
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+        <Menu  >
+          <MenuButton as={Flex} align="center" rightIcon={<ChevronDownIcon />}  w='80%' _after={{color:'red'}}>
+            <Flex border='1px solid red' justify="space-around" color='#4D4F5C' >
+              <Flex align="center">
+                <Text fontSize="14px">John Doe</Text>
+                <MdKeyboardArrowDown />
+              </Flex>
+              <Avatar size="xs" src={profilePic} />
+            </Flex>
           </MenuButton>
-          <MenuList w="22rem" border="2px soli black" mt={"1rem"}>
-            <MenuItem h="76px" _hover={{ bg: "#f5faff" }}>
-              <Flex justify="space-between" w="100%">
-                <Center w="15%">
-                  <Avatar h="2.4rem" w="2.4rem" name="Drew" src={DrewAvatar} />
-                </Center>
-                <Box w="70%" color="#43425D" p="0 .5rem">
-                  <Text
-                    fontSize="14px"
-                    letterSpacing="0px"
-                    color="#4D4F5C"
-                    fontWeight={"600"}
-                  >
-                    Bravid Kames
-                  </Text>
-                  <Text opacity="50%" fontSize="12px">
-                    Assigned you on the call with Sara
-                  </Text>
-                </Box>
-                <Box w="20%">
-                  <Text opacity="50%" fontSize="12px">
-                    2 min ago
-                  </Text>
-                </Box>
-              </Flex>
-            </MenuItem>
-            <MenuItem h="76px" _hover={{ bg: "#f5faff" }}>
-              <Flex justify="space-between" w="100%">
-                <Center w="15%">
-                  <Avatar
-                    h="2.4rem"
-                    w="2.4rem"
-                    name="Alexa"
-                    src={AlexaAvatar}
-                  />
-                </Center>
-                <Box w="70%" color="#43425D" p="0 .5rem">
-                  <Text
-                    fontSize="14px"
-                    letterSpacing="0px"
-                    color="#4D4F5C"
-                    fontWeight={"600"}
-                  >
-                    Alexa Marry
-                  </Text>
-                  <Text opacity="50%" fontSize="12px">
-                    Marked the task New UI as done
-                  </Text>
-                </Box>
-                <Box w="20%">
-                  <Text opacity="50%" fontSize="12px">
-                    5 min ago
-                  </Text>
-                </Box>
-              </Flex>
-            </MenuItem>
-            <MenuItem h="76px" _hover={{ bg: "#f5faff" }}>
-              <Flex justify="space-between" w="100%">
-                <Center w="15%">
-                  <Avatar h="2.4rem" w="2.4rem" name="Eva" src={EvaAvatar} />
-                </Center>
-                <Box w="70%" color="#43425D" p="0 .5rem">
-                  <Text
-                    fontSize="14px"
-                    letterSpacing="0px"
-                    color="#4D4F5C"
-                    fontWeight={"600"}
-                  >
-                    Eva Maria
-                  </Text>
-                  <Text opacity="50%" fontSize="12px">
-                    Added a new comment on Sales task
-                  </Text>
-                </Box>
-                <Box w="20%">
-                  <Text opacity="50%" fontSize="12px">
-                    10 min ago
-                  </Text>
-                </Box>
-              </Flex>
-            </MenuItem>
+          <MenuList>
+            <MenuItem>Download</MenuItem>
+            <MenuItem>Create a Copy</MenuItem>
+            <MenuItem>Mark as Draft</MenuItem>
+            <MenuItem>Delete</MenuItem>
+            <MenuItem>Attend a Workshop</MenuItem>
           </MenuList>
         </Menu>
       </Flex>

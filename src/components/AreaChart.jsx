c84f5fa93b66161b87a40834665645b0bc932b68
SumanJK <96579301+SumanJK@users.noreply.github.com>
2022-08-28 16:40:27 +0530

    updated area chart- complete

--- src/components/AreaChart.jsx
+++ src/components/AreaChart.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Line} from 'react-chartjs-2'
 import {MDBContainer} from 'mdbreact'
+import Chart from 'chart.js/auto';
 
 
 
--- src/components/chartData.js
+++ src/components/chartData.js
@@ -3,6 +3,7 @@ import {Line } from 'react-chartjs-2'
 import Chart from 'chart.js/auto';
 
 
+
 function Charts(props) {
   return (
     <div className="chart" style={{width: '100%', height: '20%'}}>
